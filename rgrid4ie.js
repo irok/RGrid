abdcdc4dc928b245f6a2372f8e44c17feea9e69d
Takayuki Irokawa <t.irokawa@gmail.com>
2013-05-06 17:55:06 +0900

    bugfix rgrid4ie

--- rgrid4ie.js
+++ rgrid4ie.js
@@ -1,5 +1,5 @@
 /**
- * RGrid for IE 0.0.1
+ * RGrid for IE 0.0.2
  * Licensed under the MIT license.
  * Copyright 2013 Takayuki Irokawa
  *
@@ -7,23 +7,21 @@
  */
 $(function(){
   $('.block').each(function(){
-    var $this = $(this),
-        newClass = '',
-        gridClass = {keyCount:0};
+    var $this = $(this), rgrid = {keyCount:0, baseClass:''};
     $.each($this.attr('class').split(/\s/), function(){
       if (this.match(/^(g[rmtp])[0-9]+(-[0-9]+)?$/)) {
-        gridClass[RegExp.$1] = this.substring(2);
-        gridClass.keyCount++;
+        rgrid[RegExp.$1] = 'gr'+this.substring(2);
+        rgrid.keyCount++;
       }
       else
-        newClass += ' '+this;
+        rgrid.baseClass += this+' ';
     });
-    $this.attr('class', newClass.substring(1));
-    if (gridClass.keyCount > 0)
-      $this.data('rgrid', gridClass);
+    $this.attr('class', rgrid.baseClass);
+    if (rgrid.keyCount > 0)
+      $this.data('rgrid', rgrid);
   });
 
-  var prevMedia = '',
+  var prevMedia = '';
   function adjustGrid(){
     var w = $(window).width(),
         m = w < 540 ? 'gm'
@@ -31,17 +29,9 @@ $(function(){
           :           'gp';
     if (m !== prevMedia) {
       $('.block').each(function(){
-        var $this = $(this),
-            rgrid = $this.data('rgrid'),
-            klass = $this.attr('class');
-        if (typeof rgrid === 'object') {
-          klass = klass.replace(/ ?gr[0-9]+(-[0-9]+)\b/, '');
-          if (rgrid[m])
-            klass += ' gr' + rgrid[m];
-          else if (rgrid.gr)
-            klass += ' gr' + rgrid.gr;
-          $this.attr('class', klass);
-        }
+        var $this = $(this), rgrid = $this.data('rgrid');
+        if (typeof rgrid === 'object')
+          $this.attr('class', rgrid.baseClass+(rgrid[m] || rgrid.gr || ''));
       });
       prevMedia = m;
     }
